6c2acf00b47e43d7ce9312f3bcd61c7d660c950e
hirakiuc <hirakiuc@gmail.com>
2015-07-18 05:38:10 +0900

    Initial Commit

---
+++ HatebuFeed/HatebuFeed.h
@@ -0,0 +1,19 @@
+//
+//  HatebuFeed.h
+//  HatebuFeed
+//
+//  Created by Daisuke Hirakiuchi on 2015/07/18.
+//  Copyright © 2015年 iapps.altab.jp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HatebuFeed.
+FOUNDATION_EXPORT double HatebuFeedVersionNumber;
+
+//! Project version string for HatebuFeed.
+FOUNDATION_EXPORT const unsigned char HatebuFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HatebuFeed/PublicHeader.h>
+
+
