39eda2132d72bd15f3aedea4fed859f4457005db
hirakiuc <hirakiuc@gmail.com>
2015-07-20 22:58:26 +0900

    remove HatebuFeed Parser class

--- HatebuFeed/HatebuFeedParser.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HatebuFeedParser.swift
-//  HatebuFeed
-//
-//  Created by Daisuke Hirakiuchi on 2015/07/18.
-//  Copyright © 2015年 iapps.altab.jp. All rights reserved.
-//
-
-import Foundation
