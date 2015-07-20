64cbbe3c55577e840f8b907b01190290a7f8b091
hirakiuc <hirakiuc@gmail.com>
2015-07-20 22:57:42 +0900

    add config class

--- HatebuFeed/HatebuFeedConfig.swift
+++ HatebuFeed/HatebuFeedConfig.swift
@@ -14,25 +14,26 @@ private func documentPath() -> NSString {
   return NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0]
 }
 
+private let RealmName : String = "HatebuFeed.realm"
+
 public class HatebuFeedConfig : NSObject {
   static let sharedConfig = HatebuFeedConfig()
   var realmPath : String
   var requestConfiguration : NSURLSessionConfiguration
 
   override init() {
-    self.realmPath = documentPath().stringByAppendingPathComponent("HatebuFeed.realm")
+    self.realmPath = documentPath().stringByAppendingPathComponent(RealmName)
     self.requestConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
   }
 
   public func realm() -> Realm? {
-    let config = HatebuFeedConfig.sharedConfig
     do {
       let fileManager = NSFileManager.defaultManager()
-      if fileManager.fileExistsAtPath(config.realmPath) {
-        try! fileManager.removeItemAtPath(config.realmPath)
+      if fileManager.fileExistsAtPath(self.realmPath) {
+        try! fileManager.removeItemAtPath(self.realmPath)
       }
 
-      return try Realm(path: config.realmPath)
+      return try Realm(path: self.realmPath)
     } catch {
       return nil
     }
